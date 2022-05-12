a8ed18beeb903bc36496a2e3c4ee7d44036f8125
MarioArocaPaez <73229438+MarioArocaPaez@users.noreply.github.com>
2022-05-12 20:23:08 +0200

    Add files via upload

---
+++ src/main/java/aiss/model/Gym.java
@@ -0,0 +1,61 @@
+package aiss.model;
+
+import java.util.List;
+
+public class Gym {
+	
+	private String id;
+	private String type;
+	private List<Trainer> helpers;
+	private Trainer leader;
+	
+	public Gym() {
+		
+	}
+	
+	public Gym(String id, String type, List<Trainer> helpers, Trainer leader) {
+		super();
+		this.id = id;
+		this.type = type;
+		this.helpers = helpers;
+		this.leader = leader;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public List<Trainer> getHelpers() {
+		return helpers;
+	}
+
+	public void setHelpers(List<Trainer> helpers) {
+		this.helpers = helpers;
+	}
+
+	public Trainer getLeader() {
+		return leader;
+	}
+
+	public void setLeader(Trainer leader) {
+		this.leader = leader;
+	}
+	
+	
+	
+	
+	
+
+}
